7a7baa7d48c6f5c0c2f72d3c49c6409d26c67e11
Jaredxvx <j04120100@gmail.com>
2026-04-08 13:46:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 10000; // Mas safe ang 10000 sa Render
+
+// 1. Eto yung pampagising kay Render (Web Service)
+app.get('/', (req, res) => {
+  res.send('Bot is Alive!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
